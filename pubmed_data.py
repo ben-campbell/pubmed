ebfeaa38035ef5d34cfd08b8f42b36746e72d189
Ben Campbell <ben@bccampbell.ca>
2021-01-04 05:05:00 +0000

    ingestion updates

--- pubmed_data.py
+++ pubmed_data.py
@@ -52,57 +52,72 @@ def xml_to_df(file_number):
    articles_dict = {}
    for i in tqdm(range(len(root))):
       article_dict = {}
+
       for article_title in root[i].iter('ArticleTitle'):
-         article_dict['Title'] = article_title.text
+         article_dict['Title'] = article_title.text if article_title is not None else ""
       for pubmed_date in root[i].iter('PubMedPubDate'):
+         month_text = pubmed_date.find('Month').text.zfill(2) if pubmed_date.find('Month') is not None else ""      
+         day_text = pubmed_date.find('Day').text.zfill(2) if pubmed_date.find('Day') is not None else ""
+         year_text = pubmed_date.find('Year').text if pubmed_date.find('Year') is not None else ""
          if pubmed_date.attrib['PubStatus'] == 'pubmed':
-            article_dict['Date'] = (pubmed_date.find('Month').text.zfill(2) + "/"
-                                  + pubmed_date.find('Day').text.zfill(2) + "/"
-                                  + pubmed_date.find('Year').text)
+            article_dict['Date'] = month_text + "/" + day_text + "/" + year_text
       for abstract in root[i].iter('Abstract'):
-         article_dict['Abstract'] = abstract.find('AbstractText').text
+         article_dict['Abstract'] = abstract.find('AbstractText').text if abstract.find('AbstractText') is not None else ""
       for journal in root[i].iter('ISOAbbreviation'):
-         article_dict['Journal'] = journal.text
+         article_dict['Journal'] = journal.text if journal is not None else ""
+      
+      author_string = ""
       for author_list in root[i].iter('AuthorList'):
-         author_string = ""
          for author in author_list.findall('Author'):
             lastname = author.find('LastName')
             initials = author.find('Initials')
             name = ""
             if lastname is not None:
-               name = lastname.text
+               name = lastname.text if lastname.text is not None else ""
                if initials is not None:
-                  name = name + " " + initials.text
+                  if initials.text is not None:
+                     name = name + " " + initials.text
             author_string = author_string + name + ", "
          author_string = author_string[:-2]
-         article_dict['Authors'] = author_string
-      for language in root[i].iter('Language'):      
-         article_dict['Language'] = language.text
+      article_dict['Authors'] = author_string
 
-      article_dict['Country'] = root[i].find('MedlineCitation').find('MedlineJournalInfo').find('Country').text
+      for language in root[i].iter('Language'):      
+         article_dict['Language'] = language.text if language.text is not None else ""
+
+      country = root[i].find('MedlineCitation').find('MedlineJournalInfo').find('Country')
+      article_dict['Country'] = country.text if country is not None else ""
 
       articleid_list = root[i].find('PubmedData').find('ArticleIdList')
-      for artid in articleid_list.findall('ArticleId'):
-         if artid.attrib['IdType'] == 'pubmed':
-            article_id = artid.text
-            #article_dict['ArticleID'] = articleid.text
+      if articleid_list is not None:
+         for artid in articleid_list.findall('ArticleId'):
+            if artid.attrib['IdType'] == 'pubmed':
+               article_id = artid.text if artid is not None else ""
+               #article_dict['ArticleID'] = articleid.text
       article_dict['Citations'] = []
       for ref_list in root[i].iter('ReferenceList'):
          for articleid in ref_list.iter('ArticleId'):
-            article_dict['Citations'].append(articleid.text)
+            articleid_text = articleid.text if articleid is not None else ""
+            if articleid_text is not None and articleid_text != "":
+               article_dict['Citations'].append(articleid_text)
  
       major_topics = ""
       minor_topics = ""
       for desc_name in root[i].iter('DescriptorName'):
          if desc_name.attrib['MajorTopicYN'] == 'Y':
-            major_topics = major_topics + desc_name.text + ", "
+            desc_name_text = desc_name.text if desc_name is not None else ""
+            major_topics = major_topics + desc_name_text + ", "
          elif desc_name.attrib['MajorTopicYN'] == 'N':
-            minor_topics = minor_topics + desc_name.text + ", "
+            desc_name_text = desc_name.text if desc_name is not None else ""
+            minor_topics = minor_topics + desc_name_text + ", "
       article_dict['MajorTopics'] = major_topics[:-2]
       article_dict['MinorTopics'] = minor_topics[:-2]
- 
+
+      if article_id is None or article_id == "" or int(article_id) < 0:
+         logger.warn("File number " + str(file_number) + ", missing article_id")
+         continue 
+
       if article_id in articles_dict:
-         logger.warn("Fil number " + str(file_number) + ", duplicate article_id: " + str(article_id))
+         logger.warn("File number " + str(file_number) + ", duplicate article_id: " + str(article_id))
    
       articles_dict[article_id] = article_dict
 
@@ -152,17 +167,19 @@ def download_to_db(file_number):
       logger.error("File number " + str(file_number) + ", " + str(e))
       raise PubmedException("XML parsing failed", file_number)
    try:
-      logger.info("FIle number " + str(file_number) + ", beginning neo4j ingestion")
+      logger.info("File number " + str(file_number) + ", beginning neo4j ingestion")
       df_to_db(articles_df)
-      logger.info("FIle number " + str(file_number) + ", completed neo4j ingestion")
+      logger.info("File number " + str(file_number) + ", completed neo4j ingestion")
    except Exception as e:
       logger.error("File number " + str(file_number) + ", " + str(e))
       raise PubmedException("Neo4j ingestion failed", file_number)
 
 if __name__ == "__main__":
-   try: 
-      with Pool(1) as p:
-         p.map(download_to_db, list(range(1,1063)))
-   except Exception as e:
-      logger.error("File number " + str(e.file_number) + ", incomplete, " + e.desc)
+   for file_number in range(1,1063):
+      try: 
+         #with Pool(1) as p:
+            #p.map(download_to_db, list(range(1,1063)))
+         download_to_db(file_number)
+      except Exception as e:
+         logger.error("File number " + str(e.file_number) + ", incomplete, " + e.desc)
 
